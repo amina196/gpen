02f42414ffe7881f6d151b4a439381a9c68d5e25
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2012-07-03 10:10:54 -0400

    added uploading of resume to application

--- app/controllers/jobenrollments_controller.rb
+++ app/controllers/jobenrollments_controller.rb
@@ -2,10 +2,10 @@ class JobenrollmentsController < ApplicationController
 
   def create
   	@job = Job.find(params[:jobenrollment][:job_id])
-    current_user.jobenrollments.create!(job_id: @job.id, 
-                                        resume_file_name: params[:jobenrollment][:resume].original_filename,
-                                        resume_content_type: params[:jobenrollment][:resume].content_type)
+    current_user.jobenrollments.create!(params[:jobenrollment])
     redirect_to @job
+
+    #job_id: @job.id,resume_file_name: params[:jobenrollment][:resume].original_filename, resume_content_type: params[:jobenrollment][:resume].content_type
   end
 
   def destroy
@@ -13,7 +13,7 @@ class JobenrollmentsController < ApplicationController
     current_user.unapply!(@job)
     redirect_to @job
   end
-  
+   
 end
 
 
