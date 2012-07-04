a485434cb08fedd48146372e7f29fc7754772539
George Wright <wrightgd@gmail.com>
2012-07-04 17:20:44 -0500

    drop down for states in org new form

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -65,6 +65,61 @@ class Organization < ActiveRecord::Base
       end
 
 end
+
+STATES = [
+    'AK',
+    'AL',
+    'AR',
+    'AZ',
+    'CA',
+    'CO',
+    'CT',
+    'DC',
+    'DE',
+    'FL',
+    'GA',
+    'HI',
+    'IA',
+    'ID',
+    'IL',
+    'IN',
+    'KS',
+    'KY',
+    'LA',
+    'MA',
+    'MD',
+    'ME',
+    'MI',
+    'MN',
+    'MO',
+    'MS',
+    'MT',
+    'NC',
+    'ND',
+    'NE',
+    'NH',
+    'NJ',
+    'NM',
+    'NV',
+    'NY',
+    'OH',
+    'OK',
+    'OR',
+    'PA',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VA',
+    'VT',
+    'WA',
+    'WI',
+    'WV',
+    'WY'
+  ]
+
 # == Schema Information
 #
 # Table name: organizations
