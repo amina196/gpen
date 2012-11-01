71bdeeb35cdd1eaa91f78ebc1df364d0b7a7c5cb
Justin Broglie <jbroglie@gmail.com>
2012-10-31 20:11:15 -0400

    tried using File.open(path)

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -13,7 +13,7 @@ class Notifications < ActionMailer::Base
     @job = job
     @organization = Organization.find(@job.organization_id)
     @jobenrollment = jobenrollment
-    attachments['resume.pdf'] = File.open(@jobenrollment.resume.url)
+    attachments['resume.pdf'] = File.open(@jobenrollment.resume.path)
     mail(:to => user.email, :subject => "Application on GPEN job")
   end
 
